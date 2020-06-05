c53ec915ece2dd23a5b3ad327f5093427dec5e13
Martensson35 <59262031+Martensson35@users.noreply.github.com>
2020-06-05 22:05:34 +0200

    aaasads

--- program.cs
+++ program.cs
@@ -1,1 +1,10 @@
-
+sw2.Write("\"" + radekpole[index] + "\",");
+                sw2.Write("\"" + radekpole[index+1] + "\",");
+                sw2.Write("\"" + radekpole[index+2] + "\",,,");
+                sw2.Write("\"" + radekpole[index+3] + "\",");
+                sw2.Write("\"" + radekpole[index+4] + "\",");
+                sw2.Write("\"" + radekpole[index+5] + "\",");
+                sw2.Write("\"" + radekpole[index + 6].Replace(" ", "") + "\",");
+                sw2.Write("\"" + zeme + "\",");
+                sw2.Write("\"" + radekpole[index+8] + "\",");
+                sw2.Write("\"" + radekpole[index+9] + "\",");
